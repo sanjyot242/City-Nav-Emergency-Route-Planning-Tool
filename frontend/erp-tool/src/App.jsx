bb9d93774c943186dfe71c00dac100bdb9b6e6f1
Sanjyot <41758207+sanjyot242@users.noreply.github.com>
2024-02-19 12:11:22 -0800

    Remove Old Route when chaing to and from

--- frontend/erp-tool/src/App.jsx
+++ frontend/erp-tool/src/App.jsx
@@ -5,6 +5,8 @@ import MapComponent from './Components/MapComponent';
 import { useState, useEffect } from 'react';
 import { locations, list } from './data';
 
+import CustomInput from './Components/CustomInput';
+
 function App() {
   const [position, setPosition] = useState(null);
   const [start, setStart] = useState(null);
@@ -12,11 +14,31 @@ function App() {
   const [path, setPath] = useState([]);
 
   function handleRouteCalculation() {
-    setPath([
-      { lat: 33.8704, lon: -117.9242 }, //source node
-      { lat: 33.881683, lon: -118.117012 }, //inteermediate
-      { lat: 33.77005, lon: -118.193741 }, //destination
-    ]);
+    {
+      // Find the start location in the locations array
+      const startLocation = locations.find(
+        (location) => location.name === start
+      );
+      // Find the end location in the locations array
+      const endLocation = locations.find((location) => location.name === end);
+
+      // If both start and end locations are found, set the path
+      if (startLocation && endLocation) {
+        setPath([
+          { lat: startLocation.lat, lon: startLocation.lon }, // Start node
+          { lat: 33.80497, lon: -118.0718 },
+          { lat: endLocation.lat, lon: endLocation.lon }, // End node
+        ]);
+      } else {
+        // Handle the case where the locations are not found
+        console.error('Start or end location not found');
+      }
+    }
+    // setPath([
+    //   { lat: 33.8704, lon: -117.9242 }, //source node
+    //   { lat: 33.881683, lon: -118.117012 }, //inteermediate
+    //   { lat: 33.77005, lon: -118.193741 }, //destination
+    // ]);
   }
 
   useEffect(() => {
@@ -42,50 +64,19 @@ function App() {
         <h1 className='text-4xl font-bold text-center'>City Navigation Tool</h1>
       </header>
       <div className='flex-grow flex'>
-        <div className='w-full md:w-1/4 xl:w-1/5 p-4 bg-gray-100 overflow-auto'>
-          <div className='mb-4'>
-            <label
-              htmlFor='start-point'
-              className='block text-sm font-medium text-gray-700'>
-              Select Start Point
-            </label>
-            <select
-              id='start-point'
-              className='mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md'
-              onChange={(e) => setStart(e.target.value)}>
-              <option value=''>Select Start Point</option>
-              {locations.map((location, idx) => (
-                <option key={idx} value={location.name}>
-                  {location.name}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          <div className='mb-4'>
-            <label
-              htmlFor='end-point'
-              className='block text-sm font-medium text-gray-700'>
-              Select End Point
-            </label>
-            <select
-              id='end-point'
-              className='mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md'
-              onChange={(e) => setEnd(e.target.value)}>
-              <option value=''>Select End Point</option>
-              {locations.map((location, idx) => (
-                <option key={idx} value={location.name}>
-                  {location.name}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          <button
-            className='w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-            onClick={handleRouteCalculation}>
-            Calculate Route
-          </button>
+        <div className='flex flex-col md:w-1/4 xl:w-1/5 p-4 bg-gray-100 overflow-auto'>
+          <CustomInput
+            setStart={setStart}
+            setEnd={setEnd}
+            onClick={handleRouteCalculation}
+            buttonTitle={'Calculate Route '}
+          />
+          <CustomInput
+            setStart={setStart}
+            setEnd={setEnd}
+            onClick={handleRouteCalculation}
+            buttonTitle={'Add Blockages '}
+          />
         </div>
         <div className='w-full md:w-3/4 xl:w-4/5 h-full'>
           {position && <MapComponent position={position} path={path} />}
---
+++ frontend/erp-tool/src/Components/CustomInput.jsx
@@ -0,0 +1,58 @@
+import { locations, list } from '../data';
+
+function CustomInput({ setStart, setEnd, onClick, buttonTitle }) {
+  return (
+    // <div className='w-full md:w-1/4 xl:w-1/5 p-4 bg-gray-100 overflow-auto'>
+    <>
+      <h2 className='text-center  text-xl  font-bold my-2'>
+        Plan Your Journey
+      </h2>
+      <div className='mb-4'>
+        <label
+          htmlFor='start-point'
+          className='block text-sm font-medium text-gray-700'>
+          Select Source
+        </label>
+        <select
+          id='start-point'
+          className='mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md'
+          onChange={(e) => setStart(e.target.value)}>
+          <option value=''>Select Source</option>
+          {locations.map((location, idx) => (
+            <option key={idx} value={location.name}>
+              {location.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className='mb-4'>
+        <label
+          htmlFor='end-point'
+          className='block text-sm font-medium text-gray-700'>
+          Select Destination
+        </label>
+        <select
+          id='end-point'
+          className='mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md'
+          onChange={(e) => setEnd(e.target.value)}>
+          <option value=''>Select Destination</option>
+          {locations.map((location, idx) => (
+            <option key={idx} value={location.name}>
+              {location.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <button
+        className='w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+        onClick={onClick}>
+        {buttonTitle}
+      </button>
+    </>
+    // </div>
+  );
+}
+
+export default CustomInput;
